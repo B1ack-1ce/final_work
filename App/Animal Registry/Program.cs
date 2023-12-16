560c9d3db8164f71c26fd8c8ce12a80445aac178
Artemiy <aptem64@yandex.ru>
2023-12-16 12:00:01 +0300

    complete linux tasks

---
+++ App/Animal Registry/Program.cs
@@ -0,0 +1,12 @@
+﻿using Animal_Registry.Model;
+
+namespace Animal_Registry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+        }
+    }
+}
